6d32c0012f8a8177d35cded5a812e9659561d42b
Hedo Eudae <98353469+hdae@users.noreply.github.com>
2024-02-07 01:19:51 +0900

    fix: tweak styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import DomToImage from 'dom-to-image'
-// import html2canvas from 'html2canvas'
 import { useRef, useState } from 'react'
 import styles from "./App.module.css"
 
 const Result = ({ text }: { text: string }) => {
-    const tableRef = useRef<HTMLTableElement>(null)
+    const elementRef = useRef<HTMLDivElement>(null)
     const [copied, setCopied] = useState(false)
 
     const list = text.trim().split("\n")
@@ -18,48 +17,46 @@ const Result = ({ text }: { text: string }) => {
     )
 
     return (
-        <div className={styles.Result}>
-            <h2>{title}</h2>
-            <table ref={tableRef} className={styles.Table}>
-                <thead>
-                    <tr>
-                        {head.split(/[\t\s]+/).map((item, index) => (
-                            <th key={index}>{item}</th>
-                        ))}
-                    </tr>
-                </thead>
-                <tbody>
-                    {list.map((line, index) => (
-                        <tr key={index}>
-                            {line.split(/[\t\s]+/).map((item, index) => (
-                                <td key={index}>{item}</td>
+        <>
+            <div ref={elementRef} className={styles.Result}>
+                <h2>{title}</h2>
+                <table className={styles.Table}>
+                    <thead>
+                        <tr>
+                            {head.split(/[\t\s]+/).map((item, index) => (
+                                <th key={index}>{item}</th>
                             ))}
                         </tr>
-                    ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        {list.map((line, index) => (
+                            <tr key={index}>
+                                {line.split(/[\t\s]+/).map((item, index) => (
+                                    <td key={index}>{item}</td>
+                                ))}
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
             <button role="button" onClick={async () => {
-                if (tableRef.current === null) return
+                if (elementRef.current === null) return
 
-                const blob = await DomToImage.toBlob(tableRef.current)
+                const blob = await DomToImage.toBlob(
+                    elementRef.current,
+                    {
+                        bgcolor: "white"
+                    }
+                )
+
                 const item = new ClipboardItem({ "image/png": blob })
                 await navigator.clipboard.write([item])
                 setCopied(true)
                 window.setInterval(() => setCopied(false), 3000)
-
-                // // HTML2Canvas
-                // const result = await html2canvas(tableRef.current)
-                // result.toBlob(async (blob) => {
-                //     if (blob === null) throw new Error("Failed to get image data.")
-                //     const item = new ClipboardItem({ "image/png": blob })
-                //     await navigator.clipboard.write([item])
-                //     setCopied(true)
-                //     window.setInterval(() => setCopied(false), 3000)
-                // }, "image/png")
             }}>
                 {copied ? "コピーしました！" : "画像としてコピー"}
             </button>
-        </div >
+        </>
     )
 }
 
@@ -78,10 +75,11 @@ export const App = () => {
                 ref={ref}
                 className={styles.Input}
                 onInput={() => setInput(ref.current?.value.trim())}
+                placeholder={`総得点順位\n順位  プレイヤー名  ...`}
             />
-            {
-                input?.split("\n\n").map((datum, index) => <Result key={index} text={datum} />)
-            }
+            {input?.split("\n\n").map((datum, index) => (
+                <Result key={index} text={datum} />
+            ))}
         </div>
     )
 }
